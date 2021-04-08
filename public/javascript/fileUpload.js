c1c0c3289996e759098f0dc23b64c17708a09885
ABHI <am@MacBook-Pro.local>
2021-04-08 08:56:26 -0700

    comments out filepond

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,15 +1,15 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileValidateSize,
-    FilePondPluginImageCrop,
-    FilePondPluginFileEncode,
-  )
+// FilePond.registerPlugin(
+//     FilePondPluginImagePreview,
+//     // FilePondPluginImageResize,
+//     // FilePondPluginFileValidateSize,
+//     // FilePondPluginImageCrop,
+//     // FilePondPluginFileEncode,
+//   )
   
-// FilePond.setOptions({
-//     stylePanelAspectRatio: 150 / 100,
-//     imageResizeTargetWidth: 100,
-//     imageResizeTargetHeight: 150
-// })
+// // FilePond.setOptions({
+// //     // stylePanelAspectRatio: 150 / 100,
+// //     // imageResizeTargetWidth: 100,
+// //     // imageResizeTargetHeight: 150
+// // })
 
-FilePond.parse(document.body);
+// FilePond.parse(document.body);
