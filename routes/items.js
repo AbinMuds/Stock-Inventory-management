7ba29baeb0b9df347b1ff1e353e07e4fa16d93a8
ABHI <am@MacBook-Pro.local>
2021-04-08 17:14:30 -0700

    adds edit and delete functionality for the items

--- routes/items.js
+++ routes/items.js
@@ -36,9 +36,6 @@ router.post('/:id/new', (req, res)=>{
               costPricePerPackage: req.body.costPricePerPackage,
               profileId: profile.id
       }).then((newitem) =>{
-          console.log('-----------------')
-          console.log(newitem)
-          console.log('-----------------')
           profile.addItem(newitem.dataValues.id).then(()=>{
               res.redirect(`/business/${req.params.id}`)
           })
@@ -64,6 +61,41 @@ router.get('/:id', (req,res) => {
     })
   })
 
+router.get('/delete/:id', (req,res) => {
+    db.profile.findOne({
+        where:{
+            id: req.params.id
+        }
+    }).then((profile)=>{
+        res.render("business/delete",{profile:profile})
+    }).catch((error)=>{
+        res.status(400).render('404')
+        console.log(error)
+    })
+})
+
+router.delete('/:id', (req,res) => {
+    db.profile.destroy({
+        where: {
+            id:req.params.id
+        },
+        include:[db.item]
+    }).then((deletedProfile) => {
+        db.item.destroy({
+            where : {
+                profileId: req.params.id
+            }
+        }).then((deleted)=>{
+            res.redirect("/business")
+        })
+        }).catch((error)=>{
+            res.status(400).render('404')
+            console.log(error)
+    })
+})
+
+
+
 router.get('/:id/items/:pk',(req,res) =>{
     db.profile.findOne({
         where: {
@@ -77,6 +109,59 @@ router.get('/:id/items/:pk',(req,res) =>{
         }).then((item)=>{
             res.render('items/show',{item:item,profile:profile})
         })
+    })
+})
+
+router.delete('/:id/items/:pk', (req,res) => {
+    db.item.destroy({
+        where: {
+            id:req.params.pk
+        },
+    }).then((deleted) => {
+        res.redirect(`/business/${req.params.id}`)
+    })
+})
+
+router.get('/:id/items/edit/:pk', (req,res)=> {
+    db.item.findOne({
+        where: {
+            id:req.params.pk
+        }
+    }).then((item) => {
+        res.render('items/edit',{pid:req.params.id,itemId:req.params.pk,item:item});
+    })
+})
+
+
+router.put('/:id/items/:pk', (req,res)=>{
+    const itemimage = req.files.itemimg;
+
+        itemimage.mv('public/itemimages/' + itemimage.name, function(error){
+            if(error){
+                console.log("Could.t upload image")
+                console.log(error)
+            }else{
+                console.log("Image uploaded")
+            }
+        })
+   db.item.update(
+       {
+            name: req.body.name,
+            imageLink: itemimage.name,
+            quantityOfPackage: req.body.quantityOfPackage,
+            quantityOfitemsPerPackage: req.body.quantityOfitemsPerPackage,
+            totalCP: req.body.totalCP,
+            sellingPricePerPackage: req.body.sellingPricePerPackage,
+            sellingPricePerItem: req.body.sellingPricePerItem,
+            costPricePerPackage: req.body.costPricePerPackage,
+            profileId: req.params.id
+       },
+       {where: {id:req.params.pk}}
+   ).then((updated)=> {
+       res.redirect(`/business/${req.params.id}/items/${req.params.pk}`)
+   }).catch((error)=>{
+    res.status(400).render('404')
+    console.log(error)
     })
 })
 
